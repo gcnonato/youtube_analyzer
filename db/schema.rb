cef497853c9ed6d0b389a52e3babb424573db81c
akirakudo <aki9do@gmail.com>
2015-09-13 10:12:51 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913004432) do
+ActiveRecord::Schema.define(version: 20150913010543) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.string   "price_plan"
     t.string   "url"
     t.string   "usage"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.datetime "published_at"
   end
 
 end
